5d8b0cbceca31a431a1430a780331a4493e5f5b2
jdtrinh <70670697+jdtrinh@users.noreply.github.com>
2020-10-26 11:58:28 -0700

    Add files via upload

--- LAFLA Shiny App Test Run.R
+++ LAFLA Shiny App Test Run.R
@@ -11,21 +11,63 @@
 # library(ggplot2)
 # library(ggpubr)
 # library(highcharter)
+# library(janitor)
 # library(lubridate)
 # library(plotly)
 # library(readxl)
+# library(rsconnect)
 # library(shiny)
 # library(shinydashboard)
 # library(shinythemes)
 # library(shinyWidgets)
 # library(sqldf)
+# library(stringr)
 # library(zoo)
 # library(tidyr)
 # library(devtools)
 # library(choroplethrZip)
 
+options(scipen=999)
+rm(list=ls())
 
 mydata <- read_xlsx("C:/Users/iggyt/Desktop/LAFLA/Input/Merged LAFLA Data.xlsx")
+# Clean Out Unusable Variables
+mydata <- mydata %>% mutate(
+  `Supervisorial District 1` = str_replace(mydata$`Supervisorial District 1`, "X", "1"),
+  `Supervisorial District 2` = str_replace(mydata$`Supervisorial District 2`, "X", "2"),
+  `Supervisorial District 3` = str_replace(mydata$`Supervisorial District 3`, "X", "3"),
+  `Supervisorial District 4` = str_replace(mydata$`Supervisorial District 4`, "X", "4"),
+  `Supervisorial District 5` = str_replace(mydata$`Supervisorial District 5`, "X", "5"))
+mydata$`Supervisorial District` = as.integer(coalesce(mydata$`Supervisorial District 1`,
+                                                      mydata$`Supervisorial District 2`,
+                                                      mydata$`Supervisorial District 3`,
+                                                      mydata$`Supervisorial District 4`,
+                                                      mydata$`Supervisorial District 5`))
+
+mydata <- subset(mydata, select = -c(NAME, `Zip Code.x`, `Zip Code.y`, name, zip, `Area Name`,
+                                     `Unlimited Civil (exclude Personal Injury)`,
+                                     `Unlimited Civil (Personal Injury)`,
+                                     `Family Law`, `Restraining Orders`, Probate,
+                                     `Limited Civil (exclude Collection)`,
+                                     `Limited Civil (Collection)`,
+                                     `Limited Unlawful Detainer`, `Small Claims`,
+                                     city, state, county, year, `Supervisorial District 1`,
+                                     `Supervisorial District 2`, `Supervisorial District 3`, 
+                                     `Supervisorial District 4`, `Supervisorial District 5`))
+names(mydata)
+
+
+path   <- "C:/Users/iggyt/Desktop/LAFLA/Input/"
+income <- read.csv(paste0(path, "income.csv"))
+income <- row_to_names(income, row_number = 1)
+colnames(income) <- c("id", "Name", "Median Household Income (Estimate)", "Median Household Income (MOE)")
+rent <- read.csv(paste0(path, "rent.csv"))
+rent <- row_to_names(rent, row_number = 1)
+colnames(rent) <- c("id", "Name", "Median Gross Rent (Estimate)", "Median Gross Rent (MOE)")
+tenure <- read.csv(paste0(path, "tenure.csv"))
+tenure <- row_to_names(tenure, row_number = 1)
+colnames(tenure) <- c("id", "Name", "Total (Estimate)", "Total (MOE)", "Owner Occupied (Estimate)", "Owner Occupied (MOE)",
+                      "Renter Occupied (Estimate)", "Renter Occupied (MOE)")
 
 ##################################################################################
 ###                               User Interface                               ###
@@ -50,7 +92,33 @@ ui <- dashboardPage(
   
   dashboardBody(
     tabItems(
-      tabItem(tabName = "datasets-tab"),
+      tabItem(tabName = "datasets-tab",
+              # App title ----
+              titlePanel("Rent Burden - Data Download Demo"),
+              
+              # Sidebar layout with input and output definitions ----
+              sidebarLayout(
+                
+                # Sidebar panel for inputs ----
+                sidebarPanel(
+                  
+                  # Input: Choose dataset ----
+                  selectInput("dataset", "Choose a dataset:",
+                              choices = c("Median Income", "Median Rent", "Tenure")),
+                  
+                  # Button
+                  downloadButton("downloadData", "Download")
+                  
+                ),
+                
+                # Main panel for displaying outputs ----
+                mainPanel(
+                  
+                  tableOutput("table")
+                  
+                )
+                
+              )),
       tabItem(tabName = "heatmap-tab",
               fluidPage(
                 # Set theme
@@ -108,9 +176,7 @@ ui <- dashboardPage(
                                   DT::dataTableOutput(outputId = "DT_table_a"),
                                   DT::dataTableOutput(outputId = "DT_table_b")),
                       
-                      splitLayout(cellWidths = c("50%", "50%"),
-                                  plotlyOutput(outputId = "PL_chart_a"),
-                                  plotlyOutput(outputId = "PL_chart_b"))
+                      plotlyOutput(outputId = "PL_chart")
                       
                       
                     )
@@ -173,7 +239,7 @@ server <- function(input, output, session){
   
   # Import federal housing data
   filename <- paste(DirInput, "Merged LAFLA Data (Full Joins).xlsx", sep="/")
-  masterData <- read_xlsx(filename )%>%
+  masterData <- mydata %>%
     # Make GEOID a character
     mutate(GEOID = as.character(GEOID)) 
   
@@ -185,9 +251,24 @@ server <- function(input, output, session){
   data(zip.regions)
   
   # Filter to LA Zip codes from choroplethrZip
-  LARegions <- (zip.regions %>% 
-                  filter(county.fips.numeric == 6037) %>%
-                  distinct(region))[[1]]
+  LARegions <- as.character(c(90601, 90602, 90603, 90605, 90631, 91006, 91007, 91008, 91010, 91016, 91024, 
+                              91030, 91108, 91706, 91722, 91723, 91724, 91731, 91732, 91733, 91741, 
+                              91744, 91745, 91746, 91748, 91754, 91765, 91770, 91773, 91775, 91776, 
+                              91780, 91789, 91790, 91791, 91792, 91801, 91803, 90031, 90032, 90041, 
+                              90042, 90065, 91204, 91205, 90004, 90005, 90006, 90012, 90013, 90014, 
+                              90015, 90017, 90019, 90021, 90026, 90027, 90028, 90035, 90036, 90038, 
+                              90039, 90046, 90048, 90057, 90068, 90069, 90071, 90022, 90023, 90033, 
+                              90063, 90024, 90025, 90034, 90049, 90056, 90064, 90066, 90067, 90073, 
+                              90077, 90094, 90210, 90212, 90230, 90232, 90272, 90291, 90292, 90401, 
+                              90402, 90403, 90404, 90405, 90001, 90002, 90003, 90007, 90008, 90011, 
+                              90016, 90018, 90037, 90043, 90044, 90047, 90059, 90061, 90062, 90089, 
+                              90220, 90305, 90040, 90058, 90201, 90240, 90241, 90242, 90255, 90262, 
+                              90270, 90280, 90604, 90606, 90638, 90640, 90650, 90660, 90670, 90703, 
+                              90706, 90723, 90045, 90245, 90249, 90250, 90254, 90260, 90266, 90274, 
+                              90275, 90277, 90278, 90293, 90301, 90302, 90303, 90304, 90501, 90503, 
+                              90504, 90505, 90506, 90717, 90221, 90502, 90710, 90712, 90713, 90715, 
+                              90716, 90731, 90732, 90744, 90745, 90746, 90755, 90802, 90803, 90804, 
+                              90805, 90806, 90807, 90808, 90810, 90813, 90814, 90815, 90822, 90831))
   
   # The data.frame that you provide to zip_choropleth must have one column named 
   # "region" and one column named "value". Your entries for "region" must exactly 
@@ -284,18 +365,26 @@ server <- function(input, output, session){
   select_variable_grouping <- eventReactive(input$HM_val, {
 
     if(input$HM_val == "Total Data"){
-      cols = c(1:544)
+      cols = c(2:521)
+      data <- mydata[, cols]
     } 
     else if (input$HM_val == "Demographics"){
-      cols = c(1:511)
+      cols = c(2:510)
+      data <- mydata[, cols]
     }
     else if (input$HM_val == "Housing"){
-      cols = c(512:517)
+      housing_vars  <- c("B25003", "B25070", "B25077", "B25075", 
+                         "B25064", "B25065", "B25066", "B25071", 
+                         "B25070", "B25034")
+      other_housing <- c("federallyBackedLoans", "FannieMaeOwned", "GinnieMaeOwned", 
+                         "FreddieMacOwned", "FarmerMacOwned", "nonProfitHousingCount") 
+      data <- mydata[, grepl("B25003|B25070|B25077|B25075|B25064|B25065|B25066|B25071|B25070|B25034|federallyBackedLoans|FannieMaeOwned|GinnieMaeOwned|FreddieMacOwned|FarmerMacOwned|nonProfitHousingCount", 
+                             names(mydata))]
     }
     else if (input$HM_val == "Other"){
-      cols = c(519:540)
+      cols = c(517:521)
+      data <- mydata[, cols]
     }
-    data <- mydata[, cols]
     data
   })
   
@@ -361,6 +450,68 @@ server <- function(input, output, session){
     rownames = FALSE,
   )
   
+  Var_AB <- eventReactive(c(input$HM_map_a_val, input$HM_map_b_val), {
+    # Input interested variable
+    var_A = c(input$HM_map_a_val)
+    var_B = c(input$HM_map_b_val)
+    
+    masterDataMap <- masterData %>%
+      # Filter to LA FIPS code
+      filter(GEOID %in% LARegions) %>%
+      # Only keep interested variables
+      select(GEOID, var_A, var_B)
+    
+    # Rename variables to work with package
+    names(masterDataMap) <- c("ZIP Code", input$HM_map_a_val, input$HM_map_b_val)
+    masterDataMap
+  })
+  
+  output$PL_chart <- renderPlotly({
+    plot_data = Var_AB()
+    x <- plot_data[[input$HM_map_a_val]]
+    y <- plot_data[[input$HM_map_b_val]]
+
+
+    fig <- plot_ly(data = plot_data, x = ~x, y = ~y,
+                   type = 'scatter',
+                   marker = list(size = 10,
+                                 color = 'rgba(255, 182, 193, .9)',
+                                 line = list(color = 'rgba(152, 0, 0, .8)',
+                                             width = 2)))
+    fig <- fig %>% layout(title = paste(input$HM_map_a_val, " vs. ", input$HM_map_b_val, sep=""),
+                          yaxis = list(title = input$HM_map_b_val,
+                                       zeroline = FALSE),
+                          xaxis = list(title = input$HM_map_a_val,
+                                       zeroline = FALSE))
+
+    
+    fig
+  })
+  
+  ### Aurora's Code ### 
+  
+  # Reactive value for selected dataset ----
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Median Income" = income,
+           "Median Rent" = rent,
+           "Tenure" = tenure)
+  })
+  
+  # Table of selected dataset ----
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
 }
 
 
@@ -370,3 +521,5 @@ server <- function(input, output, session){
 
 
 shinyApp(ui = ui, server = server)
+
+# rsconnect::deployApp("C:/Users/iggyt/Desktop/LAFLA/Map Shiny App/Shiny App Website/")
